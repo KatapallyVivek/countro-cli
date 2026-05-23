96094fdb584357267b977dc94adf28b1491d00e0
KatapallyVivek <vivekkatapally03@gmail.com>
2026-05-23 15:24:20 +0000

    Fix chalk import

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const { Command } = require("commander");
-const { default: chalk } = require("chalk");
+const chalk = require("chalk");
 
 const program = new Command();
 
